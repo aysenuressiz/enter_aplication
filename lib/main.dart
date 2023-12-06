06f2e812413b136889216b85911b97ae19cfc3db
aysenuressiz <21060631@stu.omu.edu.tr>
2023-12-06 12:13:15 +0300

    new

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final _usernameController = TextEditingController();
   final _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -35,83 +36,93 @@ class _LoginPageState extends State<LoginPage> {
         title: const Text('Login Page', style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Image.asset(
-                'images/flutter.png',
-                width: 200.0, // İstediğiniz genişlik
-                height: 150.0, // İstediğiniz yükseklik
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextFormField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  hintText: 'Email',
-                  labelText: 'Email',
-                  border: OutlineInputBorder(),
+      body: Form(
+        key: _formKey,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Image.asset(
+                  'images/flutter.png',
+                  width: 200.0,
+                  height: 150.0,
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: TextFormField(
-                controller: _passwordController,
-                decoration: const InputDecoration(
-                  hintText: 'Password',
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  controller: _usernameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
+                    labelText: 'Email',
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-                obscureText: true,
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: ElevatedButton(
-                onPressed: () {
-                  // Kullanıcı adı ve şifre doğrulama işlemi burada yapılır.
-                  if (_usernameController.text == 'admin@gmail.com' &&
-                      _passwordController.text == '12345') {
-                    // Giriş başarılı
-                    Navigator.pushNamed(context, '/anasayfa');
-                  } else {
-                    // Giriş başarısız
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Kullanıcı adı veya şifre hatalı.'),
-                      ),
-                    );
-                  }
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor:
-                      Colors.blue, // Buton rengi burada ayarlanıyor
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextFormField(
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
                 ),
-                child:
-                    const Text('LOGİN', style: TextStyle(color: Colors.white)),
               ),
-            ),
-            const Spacer(),
-            const Padding(
-              padding: EdgeInsets.only(top: 0.01),
-              child: Text(
-                'New User? Create Account',
-                style: TextStyle(color: Colors.black),
+              const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  'Forgot Password',
+                  style: TextStyle(color: Colors.blue),
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() == true) {
+                      // Validation successful
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                              'Email: ${_usernameController.text}\nPassword: ${_passwordController.text}'),
+                        ),
+                      );
+                    }
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text('LOG IN',
+                      style: TextStyle(color: Colors.white)),
+                ),
+              ),
+              const Spacer(),
+              const Padding(
+                padding: EdgeInsets.only(top: 0.01),
+                child: Text(
+                  'New User? Create Account',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
